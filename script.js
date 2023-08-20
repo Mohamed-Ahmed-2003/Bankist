2fefda2dbdaa356213d461175c7ee27a4f3087c1
Mohamed Ahmed <49600128+MOHAMEDAHMED1907@users.noreply.github.com>
2023-08-20 11:28:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -58,8 +58,26 @@ const account4 = {
   interestRate: 1,
   pin: 4444,
 };
+const account5 = {
+  owner: "Oday Gareeb",
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
 
-const accounts = [account1, account2];
+  movementsDates: [
+    "2021-11-01T13:15:33.035Z",
+    "2021-11-30T09:48:16.867Z",
+    "2021-12-25T06:04:23.907Z",
+    "2022-01-25T14:18:46.235Z",
+    "2022-08-25T16:33:06.386Z",
+    "2022-08-01T14:43:26.374Z",
+    "2022-08-28T18:49:59.371Z",
+    "2022-08-26T12:01:20.894Z",
+  ],
+  currency: "USD",
+  locale: "en-US",
+};
+const accounts = [account1, account2,account5];
 
 // Elements
 const labelWelcome = document.querySelector(".welcome");
